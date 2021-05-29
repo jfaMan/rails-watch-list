143a7edc1b9bd6dd00a80788ab30160bebb55e08
Allan Szewczyk <jfa_nakata@hotmail.com>
2021-05-29 17:31:58 +0900

    added about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # get 'lists/index'
   root to: 'lists#index'
+  get 'about', to: 'pages#about'
   resources :lists, only: [:index, :show, :new, :create] do
     resources :bookmarks, only: [:new, :create]
   end
